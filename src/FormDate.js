0b11388276bcfd1c43077a808b9a230ffcd4088d
jhonathanmusa <jhonathansamu91@gmail.com>
2021-07-12 21:01:43 -0500

    added filter by date

--- src/FormDate.js
+++ src/FormDate.js
@@ -5,44 +5,57 @@ import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import styles from "./FormDate.module.css";
+import { useEffect, useState } from "react";
 
 export const FormDate = (props) => {
   const { data } = props;
+  const [date, setDate] = useState([]);
+  const [filterDate, setFilterDate] = useState("");
+  const [newDate, setNewDate] = useState([]);
 
-  const filterDate = (e) => {
+  useEffect(() => {
+    setDate(data);
+  }, [data]);
+
+  const filterDateAction = (e) => {
     e.preventDefault();
-    alert("You had clicked me")
+    const newArr = date.filter((d) => d.date === filterDate);
+    setNewDate(filterDate === "" ? date : newArr);
   };
 
   return (
     <div className={styles.MainContainer}>
-      <form onSubmit={filterDate}>
+      <form onSubmit={filterDateAction}>
         <div className={styles.FormContainer}>
           <label htmlFor="meeting">Meeting Date: </label>
           <input
             className={styles.InputDate}
-            defaultValue="20-03-2019"
+            defaultValue="2019-03-20"
             type="date"
-            onChange=""
+            onChange={(e) => setFilterDate(e.target.value)}
           />
           <button className={styles.BtnFilter}>Filter</button>
         </div>
       </form>
-      <TableContainer>
+      <TableContainer >
         <Table className={styles.TableWith} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>Date:</TableCell>
               <TableCell align="right">Title:</TableCell>
               <TableCell align="right">Description:</TableCell>
+              <TableCell align="right">Amount:</TableCell>
+              <TableCell align="right">Total:</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((d) => (
+            {newDate.map((d) => (
               <TableRow key={d.id}>
                 <TableCell align="left">{d.date}</TableCell>
                 <TableCell align="right">{d.title}</TableCell>
                 <TableCell align="right">{d.description}</TableCell>
+                <TableCell align="right">{d.amount}</TableCell>
+                <TableCell align="right">{d.total}</TableCell>
               </TableRow>
             ))}
           </TableBody>
